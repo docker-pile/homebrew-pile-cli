fc393c9de69ee9ab9be9df3aa547fba94302397d
Nelson Hernandez <nelson@securitybirds.com>
2025-03-12 09:19:47 -0700

    update sha

--- Formula/pile-cli.rb
+++ Formula/pile-cli.rb
@@ -2,7 +2,7 @@ class PileCli < Formula
     desc "Docker management CLI"
     homepage "https://github.com/docker-pile/pile-cli"
     url "https://github.com/docker-pile/pile-cli/releases/download/v0.0.1/pile-darwin-amd64"
-    sha256 "f45d4ddad9eef5c029442e95054bc90ee19741a0d4fba194eb97fae490418d31"
+    sha256 "242631fb8d21471d4947ab63090dbf3d3ee694af4a73af80e59ecb3b6c71ba5e"
     license "MIT"
   
     def install
