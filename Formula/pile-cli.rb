c0017b45e2e67c36ab52e846d8fdf7211cd0d9b7
Nelson Enzo <nelsonh@gmail.com>
2025-03-15 06:26:08 -0700

    v0.0.4

--- Formula/pile-cli.rb
+++ Formula/pile-cli.rb
@@ -1,8 +1,8 @@
 class PileCli < Formula
     desc "Docker management CLI"
     homepage "https://github.com/docker-pile/pile-cli"
-    url "https://github.com/docker-pile/pile-cli/releases/download/v0.0.3/pile-darwin-amd64"
-    sha256 "1a43fed1ee47029ebaf2a237bbc99c2e7aab1608ee14a54ce17a1874f5cca758"
+    url "https://github.com/docker-pile/pile-cli/releases/download/v0.0.4/pile-darwin-amd64"
+    sha256 "1f85da2895ba53f4cd1194f73f6bbe5b2823eeb0339b8ee06dd0168d5dc46225"
     license "MIT"
   
     def install
